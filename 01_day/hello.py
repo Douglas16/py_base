b775c44197359ff8d311393efe2318278bfd75d4
Douglas Desiderio <douglas.salvador@gmail.com>
2023-05-17 13:45:53 -0300

    alter hello.py SHEBANG

--- 01_day/hello.py
+++ 01_day/hello.py
@@ -1,3 +1,33 @@
-print('meu primeiro nome'.upper())
-print(1973 - 2023)
+#!/usr/bin/env python3
+
+'''
+
+Filosofia da Linguagem ( Zen of Python ) PEP 20
+
+Bonito é melhor que feio.
+Explícito e melhor que implícito.
+Simples e melhor que Complexo.
+Complexo é melhor que Complicado.
+Linear é melhor do que Aninhado.
+Esparso é melhor que Denso.
+Legibilidade Conta.
+Casos especiais não são especiais o bastante para quebrar as regras.
+Anda que praticidade vença a pureza.
+Erros nunca devem passar sileciosamente.
+A menos que sejam explicitamente silencioados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deveria haver um - E prefrencialmente só um - Modo óbvio para fazer algo
+Embora esse modo possa não ser óbvio a princípio, a menos que você seja holondês.
+Agora é melhor que nunca, embora nunca frequenctemente seja melhor que imediatemente.
+Se a implementação é dificil de explicar, é uma má ideia. se for fácil de explicar, pode ser uma boa ideia.
+
+
+'''
+print("hello")
+
+
+
+
+
+
 
